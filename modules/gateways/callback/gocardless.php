7b114c25a232b86210e66a7312ea6737e7168874
Maartje Eyskens <meyskens@me.com>
2017-06-23 21:59:27 +0200

    add code

---
+++ modules/gateways/callback/gocardless.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Copyright (c) 2017, Maartje Eyskens at The Innovating Group LLP
+All rights reserved.
+*/
+
+// Require libraries needed for gateway module functions.
+use WHMCS\Database\Capsule;
+require_once __DIR__ . '/../../../init.php';
+require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
+require_once __DIR__ . '/../../../includes/invoicefunctions.php';
+require_once __DIR__ . '/../../../modules/gateways/gocardless.php';
+
+// Fetch gateway configuration parameters.
+$gatewayParams = getGatewayVariables("gocardless");
+// Die if module is not active.
+if (!$gatewayParams['type']) {
+    die("Module Not Activated");
+}
+
+$headers = getallheaders();
+
+// Handle the redirect flow callback
+if (!empty($_GET["redirect_flow_id"])) {
+    $gocardless = getConfiguredGoCardless($gatewayParams);
+    $redirectFlow = $gocardless->redirectFlows()->complete($_GET["redirect_flow_id"],
+    [
+        "params" => ["session_token" => $_SESSION["gc_session_token"]]
+    ]);
+    update_query(
+        "tblclients",
+        array(
+            'cardtype' => "GoCardless",
+            "gatewayid" => $redirectFlow->links->mandate
+        ),
+        array("id" => $_SESSION["gc_params"]['clientdetails']["userid"])
+    );
+
+    header("Location: " . $gatewayParams["systemurl"] . "/viewinvoice.php?id=" . $_SESSION["gc_params"]['invoiceid']);
+
+    unset($_SESSION["gc_session_token"]);
+    unset($_SESSION["gc_params"]);
+    exit();
+}
+
+// Handle payment
+if (!empty($_GET["paynow"])) {
+    if (!array_key_exists("gc_params", $_SESSION)) {
+        die("Invalid transaction");
+    }
+    $params = $_SESSION["gc_params"];
+    $gocardless = getConfiguredGoCardless($gatewayParams);
+
+    $token = bin2hex(openssl_random_pseudo_bytes(64));
+
+    $userInfo = Capsule::table('tblclients')
+        ->where('id', $params['clientdetails']["userid"])
+        ->get();
+    
+    if (empty($userInfo[0]->gatewayid) && $userInfo[0]->cardtype != "GoCardless") {
+        die("Invalid transaction");
+    }
+
+    $payment = $gocardless->payments()->create([
+        "params" => [
+            "amount" => floatval($params['amount']) * 100,
+            "currency" => $params['currency'],
+            "links" => [
+                "mandate" => $userInfo[0]->gatewayid
+            ],
+            "metadata" => [
+                "invoice_number" => (string)$params['invoiceid']
+            ]
+        ],
+        "headers" => [
+            "Idempotency-Key" => $token
+        ]
+    ]);
+  
+    $fee = $params['amount'] / 100 * 1;
+    if ($fee < 0.20) {
+        $fee = 0.20;
+    } else if ($fee > 2) {
+        $fee = 2;
+    }
+
+    addInvoicePayment($params['invoiceid'],$payment->id,$params['amount'],$fee,"gocardless");
+
+    header("Location: " . $params["systemurl"] . "/viewinvoice.php?id=" . $params['invoiceid']);
+    
+    unset($_SESSION["gc_session_token"]);
+    unset($_SESSION["gc_params"]);
+
+    exit();
+}
+
+// Handle webhooks
+if (!empty($headers["Webhook-Signature"])) {
+    $token = $gatewayParams["webhookSignature"];
+    $raw_payload = file_get_contents('php://input');
+    $provided_signature = $headers["Webhook-Signature"];
+    $calculated_signature = hash_hmac("sha256", $raw_payload, $token);
+    if ($provided_signature != $calculated_signature) {
+        header("HTTP/1.1 498 Invalid Token");
+        exit();
+    }
+    $payload = json_decode($raw_payload, true);
+
+    // Each webhook may contain multiple events to handle, batched together
+    foreach ($payload["events"] as $event) {
+        print("Processing event " . $event["id"] . "\n");
+
+        switch ($event["resource_type"]) {
+            case "mandates":
+                gocardless_process_mandate_event($event);
+            break;
+            case "payments":
+                gocardless_process_payment_event($event);
+            break;
+            default:
+                print("Don't know how to process an event with resource_type " . $event["resource_type"] . "\n");
+                break;
+         }
+    }
+    exit();
+}
---
+++ modules/gateways/gocardless.php
@@ -0,0 +1,138 @@
+<?php
+/*
+Copyright (c) 2017, Maartje Eyskens at The Innovating Group LLP
+All rights reserved.
+*/
+
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+
+use WHMCS\Database\Capsule;
+require "gocardless/vendor/autoload.php";
+
+function gocardless_MetaData()
+{
+    return array(
+        'DisplayName' => 'GoCardless Gateway Module',
+        'APIVersion' => '1.0', // Use API Version 1.1
+        'DisableLocalCredtCardInput' => true,
+        'TokenisedStorage' => true,
+    );
+}
+
+function gocardless_config() {
+    return [
+        'FriendlyName' => ['Type' => 'System', 'Value' => 'GoCardless'],
+        'accessToken' => ['FriendlyName' => 'Access Token', 'Type' => 'text', 'Size' => '40'],
+        'webhookSignature' => ['FriendlyName' => 'Webhook Signature', 'Type' => 'text', 'Size' => '40'],
+        'sandbox' => ["FriendlyName" => "Sandbox", "Type" => "yesno"]
+    ];
+}
+
+function getConfiguredGoCardless($params) {
+     return new \GoCardlessPro\Client(array(
+        'access_token' => $params['accessToken'],
+        'environment'  => ($params['sandbox'] === 'on') ? \GoCardlessPro\Environment::SANDBOX : \GoCardlessPro\Environment::LIVE 
+    ));
+}
+
+function gocardless_link($params) {
+    $gocardless = getConfiguredGoCardless($params);
+    $output = "";
+    $hasMandate = false;
+
+    if (!in_array($params['currency'], ["GBP", "EUR", "SEK"])) {
+        return "You currency is currently not supported";
+    }
+
+    $userInfo = Capsule::table('tblclients')
+        ->where('id', $params['clientdetails']["userid"])
+        ->get();
+    
+    if (!empty($userInfo[0]->gatewayid) && $userInfo[0]->cardtype == "GoCardless") {
+        $hasMandate = true;
+        $output .= '<a href="' .  $params['systemurl'] . '/modules/gateways/callback/gocardless.php?paynow=true' . '"><button class="btn btn-success">' . $params["langpaynow"] . '</button></a></br></br>';
+    }
+    
+    $_SESSION["gc_session_token"] = $token = bin2hex(openssl_random_pseudo_bytes(64)); // generate randon secure token
+    $_SESSION["gc_params"] = $params;
+
+    $redirectFlow = $gocardless->redirectFlows()->create(
+        [
+            "params" => [
+                "session_token" => $_SESSION["gc_session_token"],
+                "success_redirect_url" => $params['systemurl'] . '/modules/gateways/callback/gocardless.php',
+                "prefilled_customer" => [
+                    "given_name" => $params['clientdetails']['firstname'],
+                    "family_name" => $params['clientdetails']['lastname'],
+                    "email" => $params['clientdetails']['email'],
+                    "address_line1" => $params['clientdetails']['address1'],
+                    "address_line2" => $params['clientdetails']['address2'],
+                    "city" => $params['clientdetails']['city']
+                ]   
+            ]
+        ]
+    );
+
+    $output .= '<a href="' . $redirectFlow->redirect_url . '"><button class="btn btn-primary">' . ($hasMandate ? "Re-Setup" : "Setup") .' Direct Debit</button></a>';
+    return $output;
+}
+
+
+function gocardless_refund($params) {
+    $gocardless = getConfiguredGoCardless($params);
+    $payment = $gocardless->payments()->cancel($params['transid']);
+    return array(
+        'status' => 'success',
+        // Data to be recorded in the gateway log - can be a string or array
+        'rawdata' => $payment
+    );
+}
+
+function gocardless_process_mandate_event($event) {
+    switch ($event["action"]) {
+        case "failed":
+        case "expired":
+        case "resubmission_requested":
+        case "cancelled":
+            print("Removing " . $event["links"]["mandate"] . "!\n");
+
+            Capsule::table('tblclients')
+                ->where('gatewayid', $event["links"]["mandate"])
+                ->update(['gatewayid' => '']);
+            break;
+        default:
+            print("Don't know how to process a mandate " . $event["action"] . " event\n");
+            break;
+  }
+}
+
+function gocardless_process_payment_event($event) {
+    $gocardless = getConfiguredGoCardless(getGatewayVariables("gocardless"));
+    switch ($event["action"]) {
+        case "failed":
+        case "charged_back":
+            print("Removing " . $event["links"]["payment"] . "!\n");
+            $transactions = localAPI("GetTransactions", ["transid" => $event["links"]["payment"]], "API");
+            if (count($transactions["transactions"]["transaction"]) < 1) {
+                return;
+            }
+
+            localAPI("UpdateTransaction", [
+                'transactionid' => $transactions["transactions"]["transaction"][0]["id"],
+                'amountin' => 0.0,
+                'fees' => 0.0
+            ], "API");
+
+            localAPI("UpdateInvoice", array(
+                'invoiceid' => $transactions["transactions"]["transaction"][0]["invoiceid"],
+                'status' => 'Unpaid'
+            ), "API");
+
+            break;
+        default:
+            print("Don't know how to process a mandate " . $event["action"] . " event\n");
+            break;
+  }
+}
